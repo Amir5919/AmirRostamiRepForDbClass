5f4f4cdff8bdc444707c85b2516407f5f86594ea
Amir5919 <161971340+Amir5919@users.noreply.github.com>
2024-03-07 10:43:11 +0330

    Delete addUser.php

--- addUser.php
+++
@@ -1,7 +0,0 @@
-<?php
-function addUser($dbc){
-$sql = "INSERT INTO users (name,family,pass,phone) VALUES ( 'Amir' , 'Rostami' , '1234','09929876543') , ( 'Javad' , 'Torabi' , '1400','09132456789') , ( 'Reza' , 'Pourmiri' , '4321' , '09038765432')";
-$result = $dbc -> query ($sql);
-return $result;
-}
-?>
