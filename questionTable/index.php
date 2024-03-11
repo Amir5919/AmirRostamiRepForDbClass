db74cc57b0686ee7f08a2b9eb825a880da0d2f88
Amir5919 <161971340+Amir5919@users.noreply.github.com>
2024-03-11 08:55:27 +0330

    Create index.php

---
+++ questionTable/index.php
@@ -0,0 +1,78 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AmirRostami</title>
+</head>
+<body>
+    
+<?php
+
+include 'dbConnect.php';
+
+$dbc = new mysqli($serverName , $userName , $password );
+$dbc -> select_db($dbName);
+
+if($dbc -> connect_error){
+    die("connection failed: ". $dbc -> connect_error);
+    echo "<br>";
+}
+
+include 'addQuestion.php';
+
+$result = addQuestion($dbc);
+
+if($result){
+    echo "addQuestion query executed successfully!";
+    echo "<br>";
+}
+else{
+    echo "addQuestion query has an error: ".$dbc -> error;
+    echo "<br>";
+}
+
+include 'deleteQuestion.php';
+
+$result = deleteQuestion($dbc);
+
+if($result){
+    echo "deleteQuestion query executed successfully!";
+    echo "<br>";
+}
+else{
+    echo "deleteQuestion query has an error: ".$dbc -> error;
+    echo "<br>";
+}
+
+include 'selectQuestion.php';
+
+$result = selectQuestion($dbc);
+$table;
+$jsonTable;
+
+if($result->num_rows>0){
+    echo "selectQuestion query executed successfully!";
+    $table=$result->fetch_all();
+    $jsonTable=json_encode($table);
+    echo "<br>";
+}
+else{
+    echo "0 result";
+    echo "<br>";
+}
+
+var_dump($jsonTable);
+
+
+
+
+
+$dbc -> close();
+
+?>
+
+
+
+
+</body>
+</html>
